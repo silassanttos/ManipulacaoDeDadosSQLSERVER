0d0a97041cd20d0d6918a1b5d1138b417611bc4d
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-08-21 22:32:56 -0300

    Update create_database.sql

--- DBExemplo/create_database.sql
+++ DBExemplo/create_database.sql
@@ -1,1 +1,13 @@
+-- Drop database
+IF DB_ID('DBExemplo') IS NOT NULL begin 
+   ALTER DATABASE DBExemplo set single_user with rollback immediate; 
+   DROP DATABASE DBExemplo;
+end 
+
+-- Create database
+CREATE DATABASE DBExemplo;
+GO
+
+USE DBExemplo;
+GO
 
